39f27dabdb32363a8a67303e9cdc97411181738a
ChachyDev <shalomadecoolboy@outlook.com>
2021-08-28 19:04:04 +0100

    Fix possible compatibility issues

--- src/main/java/club/chachy/lazylanguageloader/client/mixin/MixinReloadableResourceManagerImpl.java
+++ src/main/java/club/chachy/lazylanguageloader/client/mixin/MixinReloadableResourceManagerImpl.java
@@ -4,25 +4,25 @@ import club.chachy.lazylanguageloader.client.state.StateManager;
 import net.minecraft.client.resource.language.LanguageManager;
 import net.minecraft.resource.ReloadableResourceManagerImpl;
 import net.minecraft.resource.ResourceReloader;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Collections;
-import java.util.List;
 
 @Mixin(ReloadableResourceManagerImpl.class)
 public class MixinReloadableResourceManagerImpl {
-    @Shadow
-    @Final
-    private List<ResourceReloader> reloaders;
-
-    @Redirect(method = "reload", at = @At(value = "FIELD", target = "Lnet/minecraft/resource/ReloadableResourceManagerImpl;reloaders:Ljava/util/List;"))
-    private List<ResourceReloader> init(ReloadableResourceManagerImpl impl) {
+    @ModifyArg(
+        method = "reload",
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/google/common/collect/Lists;newArrayList(Ljava/lang/Iterable;)Ljava/util/ArrayList;",
+            remap = false
+        )
+    )
+    private Iterable<ResourceReloader> reload(Iterable<ResourceReloader> reloaders) {
         if (StateManager.isResourceLoadViaLanguage()) {
             return Collections.singletonList(StateManager.getLanguageManager());
         }
