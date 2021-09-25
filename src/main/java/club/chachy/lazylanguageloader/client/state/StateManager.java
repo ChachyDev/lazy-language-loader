685f81dd4a44a25cdb5cce3a2ebdf00d549a6af5
ChachyDev <shalomadecoolboy@outlook.com>
2021-09-25 08:02:49 +0100

    [skip ci] Add documentation about bypassing the "restricted" reloaders
    (This is for developers)

--- src/main/java/club/chachy/lazylanguageloader/client/state/StateManager.java
+++ src/main/java/club/chachy/lazylanguageloader/client/state/StateManager.java
@@ -21,7 +21,13 @@ public class StateManager {
         return reloaders;
     }
 
-    public static void addResourceReloader(ResourceReloader manager) {
-        reloaders.add(manager);
+    /**
+     * If any developer wants to workaround lazy-language-loader you could depend on it via Jitpack and add your resource reloader here
+     * if not it will not be reloaded. Sadly if your resource reloader doesn't derive from SearchManager or LanguageManager there isn't much
+     * more I can do to determine that you do stuff with languages...
+     * @param reloader Reloader to be used on language reloads
+     */
+    public static void addResourceReloader(ResourceReloader reloader) {
+        reloaders.add(reloader);
     }
 }
