65029f87a8039e0d2c5fa9d3f87dfe39c416ff5b
ChachyDev <shalomadecoolboy@outlook.com>
2021-12-01 07:49:26 +0000

    Pick StateManager changes

--- src/main/java/club/chachy/lazylanguageloader/client/impl/state/StateManager.java
+++ src/main/java/club/chachy/lazylanguageloader/client/impl/state/StateManager.java
@@ -5,7 +5,7 @@ import club.chachy.lazylanguageloader.client.impl.language.CodeLanguageMatcher;
 import club.chachy.lazylanguageloader.client.impl.language.NameLanguageMatcher;
 import club.chachy.lazylanguageloader.client.impl.language.RegionLanguageMatcher;
 import net.minecraft.client.resource.language.LanguageDefinition;
-import net.minecraft.client.resource.language.LanguageManager;
+import net.minecraft.resource.ResourceReloader;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,7 +14,7 @@ import java.util.List;
 public class StateManager {
     private static final List<LanguageMatcher> languageMatchers = new ArrayList<>();
     private static boolean resourceLoadViaLanguage = false;
-    private static LanguageManager languageManager = null;
+    private static final List<ResourceReloader> reloaders = new ArrayList<>();
 
     static {
         languageMatchers.addAll(Arrays.asList(new CodeLanguageMatcher(), new NameLanguageMatcher(), new RegionLanguageMatcher()));
@@ -28,15 +28,19 @@ public class StateManager {
         StateManager.resourceLoadViaLanguage = resourceLoadViaLanguage;
     }
 
-    public static LanguageManager getLanguageManager() {
-        if (languageManager == null) {
-            throw new RuntimeException("The Language manager was accessed too early or was never set... Please report this to the developer");
-        }
-        return languageManager;
+    public static List<ResourceReloader> getResourceReloaders() {
+        return reloaders;
     }
 
-    public static void setLanguageManager(LanguageManager languageManager) {
-        StateManager.languageManager = languageManager;
+    /**
+     * If any developer wants to workaround lazy-language-loader you could depend on it via Jitpack and add your resource reloader here
+     * if not it will not be reloaded. Sadly if your resource reloader doesn't derive from SearchManager or LanguageManager there isn't much
+     * more I can do to determine that you do stuff with languages...
+     *
+     * @param reloader Reloader to be used on language reloads
+     */
+    public static void addResourceReloader(ResourceReloader reloader) {
+        reloaders.add(reloader);
     }
 
     public static boolean isMatchable(String input, LanguageDefinition definition) {
