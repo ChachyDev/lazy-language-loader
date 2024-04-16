8e89b84a40f3731c667e1b67befbb6e42d873f90
ChachyDev <shaloma123@protonmail.com>
2024-04-16 18:58:53 +0100

    fix: discard splash overlay on crafting easter egg language update (fixes #12)

--- src/main/java/dev/chachy/lazylanguageloader/client/mixin/optimizations/splash/MixinMinecraftClient.java
+++ src/main/java/dev/chachy/lazylanguageloader/client/mixin/optimizations/splash/MixinMinecraftClient.java
@@ -4,10 +4,12 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Overlay;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.SplashOverlay;
+import net.minecraft.client.gui.screen.ingame.CraftingScreen;
 import net.minecraft.client.gui.screen.option.LanguageOptionsScreen;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,8 +22,13 @@ public class MixinMinecraftClient {
 
     @Inject(method = "setOverlay", at = @At("HEAD"), cancellable = true)
     private void lazyLanguageLoader$$setOverlay(Overlay overlay, CallbackInfo ci) {
-        if (overlay instanceof SplashOverlay && currentScreen instanceof LanguageOptionsScreen) {
+        if (overlay instanceof SplashOverlay && lazyLanguageLoader$$verifyScreen(currentScreen)) {
             ci.cancel();
         }
+    }
+
+    @Unique
+    private boolean lazyLanguageLoader$$verifyScreen(Screen screen) {
+        return screen instanceof LanguageOptionsScreen || screen instanceof CraftingScreen;
     }
 }
